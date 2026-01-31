02bc526939ff9abc33f1a523e012d23e16e89dda
sshivambe24-source <sshivam_be24@thapar.edu>
2026-01-31 22:13:32 +0530

    Create Q5.cpp

---
+++ Assignment 1/Q5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << "Hello\nWorld\n";
+    cout << "Shivam\tN\n";
+    cout << "Shivam\bK\n";
+    cout << setw(10) << "Shivam";
+    return 0;
+}
