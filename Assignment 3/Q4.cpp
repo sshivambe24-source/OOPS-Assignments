4a44458af899c6c7a5186539a4abff16d214b3f2
sshivambe24-source <sshivam_be24@thapar.edu>
2026-02-16 16:11:57 +0530

    Create Q4.cpp

---
+++ Assignment 3/Q4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class A {
+    int x;
+public:
+    A() { x = 100; }
+    friend class B;  // B is friend class
+};
+class B {
+public:
+    void show(A a) {
+        cout << "Value of x: " << a.x << endl;
+    }
+};
+int main() {
+    A obj1;
+    B obj2;
+    obj2.show(obj1);
+    return 0;
+}
